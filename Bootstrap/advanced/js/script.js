75c9bbac927fff6a8e446eda065573fa81dd2e21
yuuka <hosininegaiwo.1905@gmail.com>
2024-02-01 18:42:57 +0900

    2/1 18:42

--- Bootstrap/advanced/js/script.js
+++ Bootstrap/advanced/js/script.js
@@ -1,11 +1,12 @@
 const year = document.querySelector("#year");
 const date = new Date();
+
 year.textContent = date.getFullYear();
 
 console.dir(date);
-console.log(date.getFullYear); //年
-console.log(date.getDay); // 曜日
-console.log(date.getHours); // 月
+console.log(date.getFullYear()); //年
+console.log(date.getDay()); //曜日
+console.log(date.getHours()); //時
 
 // year.innerHTML = "あいうえお";
 // year.innerText = "かきくけこ";
