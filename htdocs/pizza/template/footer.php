d2ac1ce3d15dcaffc52ca631881e5e9e9eab52d7
k_kayo <148745558+k-kayo@users.noreply.github.com>
2024-02-15 16:59:15 +0900

    Add files via upload

---
+++ htdocs/pizza/template/footer.php
@@ -0,0 +1,9 @@
+  <footer class="text-center my-5">
+    <small>&copy; <?= date('Y'); ?> pizzeria interplan</small>
+  </footer>
+
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.jsh"></script>
+  <script src="js/script.js"></script>
+  </body>
+
+  </html>
