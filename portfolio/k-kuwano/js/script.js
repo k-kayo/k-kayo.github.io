cb5845c90d2fcf28f86966624363d5f7bbcdde84
yuuka <hosininegaiwo.1905@gmail.com>
2024-03-07 18:52:51 +0900

    pcサイト画像サイズ調整

--- portfolio/k-kuwano/js/script.js
+++ portfolio/k-kuwano/js/script.js
@@ -19,16 +19,6 @@ $(function () {
 
 });
 
-
-//2. 確認を促すモーダル
-// $(".confirm").modaal({
-//   type:'confirm',
-//   confirm_title: 'ログイン画面',//確認画面タイトル
-//   confirm_button_text: 'ログイン', //確認画面ボタンのテキスト
-//   confirm_cancel_button_text: 'キャンセル',//確認画面キャンセルボタンのテキスト
-//   confirm_content: 'ログインが必要です。この画面はボタンを押さなければ閉じません。',//確認画面の内容
-// });
-
 //3. 画像のモーダル
 $(".gallery").modaal({
 	type: 'image',
