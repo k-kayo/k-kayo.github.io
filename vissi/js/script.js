58f7355b78cfa06c5dc208eb6c363f576c4efa81
yuuka <hosininegaiwo.1905@gmail.com>
2024-03-20 23:03:43 +0900

    3/20 コンセプトまで完成

--- vissi/js/script.js
+++ vissi/js/script.js
@@ -6,16 +6,18 @@ $(".mv-area").slick({
   swipe: false, //👈 スマホでスワイプした時にスワイプしない
 });
 
-// $(function () {
-//   $(".mv-area").slick({
-//     autoplay: true,
-//     autoplaySpeed: 4000,
-//     infinite: true,
-//     speed: 1200,
-//     fade: true,
-//     cssEase: "linear",
-//     adaptiveHeight: true,
-//     arrows: false, // 👈前後の矢印
-//     swipe: false, //👈 スマホでスワイプした時にスワイプしない
-//   });
-// });
+$(".slider").slick({
+  autoplay: true, //自動的に動き出すか。初期値はfalse。
+  autoplaySpeed: 3000, //次のスライドに切り替わる待ち時間
+  speed: 1000, //スライドの動きのスピード。初期値は300。
+  infinite: true, //スライドをループさせるかどうか。初期値はtrue。
+  slidesToShow: 1, //スライドを画面に3枚見せる
+  slidesToScroll: 1, //1回のスクロールで3枚の写真を移動して見せる
+  arrows: true, //左右の矢印あり
+  prevArrow: '<div class="slick-prev"></div>', //矢印部分PreviewのHTMLを変更
+  nextArrow: '<div class="slick-next"></div>', //矢印部分NextのHTMLを変更
+  dots: true, //下部ドットナビゲーションの表示
+  pauseOnFocus: false, //フォーカスで一時停止を無効
+  pauseOnHover: false, //マウスホバーで一時停止を無効
+  pauseOnDotsHover: false, //ドットナビゲーションをマウスホバーで一時停止を無効
+});
