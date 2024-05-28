34987c8e43d48390cc5b82232ae424200803c88d
yuuka <hosininegaiwo.1905@gmail.com>
2024-05-28 12:48:24 +0900

    5/28 修正分1

--- portfolio/js/script.js
+++ portfolio/js/script.js
@@ -1,5 +1,4 @@
 $(function () {
-
   //ページ内スクロール
   var navHeight = $(".header").outerHeight();
 
@@ -7,35 +6,37 @@ $(function () {
     var href = $(this).attr("href");
     var target = $(href == "#" || href == "" ? "html" : href);
     var position = target.offset().top - navHeight;
-    $("html, body").animate({ scrollTop: position, }, 300, "swing");
+    $("html, body").animate({ scrollTop: position }, 300, "swing");
     return false;
   });
 
   //ページトップ
   $("#js-page-top").on("click", function () {
-    $("body,html").animate({ scrollTop: 0, }, 300);
+    $("body,html").animate({ scrollTop: 0 }, 300);
     return false;
   });
-
 });
 
 //3. 画像のモーダル
 $(".gallery").modaal({
-	type: 'image',
-	overlay_close:true,//モーダル背景クリック時に閉じるか
-	before_open:function(){// モーダルが開く前に行う動作
-		$('html').css('overflow-y','hidden');/*縦スクロールバーを出さない*/
-	},
-	after_close:function(){// モーダルが閉じた後に行う動作
-		$('html').css('overflow-y','scroll');/*縦スクロールバーを出す*/
-	}
+  type: "image",
+  overlay_close: true, //モーダル背景クリック時に閉じるか
+  before_open: function () {
+    // モーダルが開く前に行う動作
+    $("html").css("overflow-y", "hidden"); /*縦スクロールバーを出さない*/
+  },
+  after_close: function () {
+    // モーダルが閉じた後に行う動作
+    $("html").css("overflow-y", "scroll"); /*縦スクロールバーを出す*/
+  },
 });
 
-$(function(){//alert();
+$(function () {
+  //alert();
   var qrtext = "ポートフォーリオサイトQRコード";
   var utf8qrtext = unescape(encodeURIComponent(qrtext));
   $("#img-qr").html("");
-  $("#img-qr").qrcode({text:utf8qrtext});
+  $("#img-qr").qrcode({ text: utf8qrtext });
 });
 
 var canvas = $("#img-qr canvas");
