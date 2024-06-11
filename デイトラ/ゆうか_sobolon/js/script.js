77f2479dd29785c138f8957fddf16ab09c4a566a
yuuka <hosininegaiwo.1905@gmail.com>
2024-06-12 07:22:10 +0900

    6/12

--- デイトラ/ゆうか_sobolon/js/script.js
+++ デイトラ/ゆうか_sobolon/js/script.js
@@ -3,3 +3,20 @@ jQuery("#js-drawer-icon").on("click", function (e) {
   jQuery("#js-drawer-icon").toggleClass("is-checked");
   jQuery("#js-drawer-content").toggleClass("is-checked");
 });
+
+// スクロールに応じて要素を「フワッ」と登場させる/IntersectionObserverを使った実装例//
+const intersectionObserver = new IntersectionObserver(function (entries) {
+  entries.forEach(function (entry) {
+    if (entry.isIntersecting) {
+      entry.target.classList.add("is-in-view");
+    } else {
+      // entry.target.classList.remove("is-in-view");
+      // 👆コメントアウト外すと何回も表示される。コメントアウトしておくと表示は１回のみ
+    }
+  });
+});
+
+const inViewItems = document.querySelectorAll(".js-in-view");
+inViewItems.forEach(function (inViewItem) {
+  intersectionObserver.observe(inViewItem);
+});
