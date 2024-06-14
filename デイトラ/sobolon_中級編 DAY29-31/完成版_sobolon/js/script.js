d07491cc9d976db9c1c3e556856b4694dede5dcc
yuuka <hosininegaiwo.1905@gmail.com>
2024-06-15 02:30:13 +0900

    6/15 レスポンシブ確認まで

--- デイトラ/sobolon_中級編 DAY29-31/完成版_sobolon/js/script.js
+++
@@ -1,6 +0,0 @@
-document.querySelector("#js-drawer-button").addEventListener("click", function (e) {
-  e.preventDefault();
-
-  document.querySelector("#js-drawer-button").classList.toggle("is-checked");
-  document.querySelector("#js-drawer-content").classList.toggle("is-checked");
-});
